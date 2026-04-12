bf5c1ab389f425b2839105bcc54a896987bfe5c2
Muhammad Zohaib Malik <muhammadzohaibmalik10@gmail.com>
2026-04-12 20:34:04 +0500

    feat: add SocketContext for socket.io integration and update dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SocketProvider } from './Context/SocketContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <SocketProvider>
+    <App/>
+  </SocketProvider>
 )
