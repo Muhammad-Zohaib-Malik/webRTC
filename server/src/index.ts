2ada7140a3728718f6e31903ee080af516923e0a
Muhammad Zohaib Malik <muhammadzohaibmalik10@gmail.com>
2026-04-12 19:11:19 +0500

    setup socket io and cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-console.log("hello")
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+
+});
+
+
+
+server.listen(5000, () => {
+    console.log('Server is running on port 5000');
+})
